ddf982c36f6d942ff1106c8d02e5aa20498d8977
Pankaj Jha <54464523+pankajjha-04@users.noreply.github.com>
2023-11-16 20:01:54 +0530

    Update CurrentAccount.java

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,14 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount{
-    String tradeLicenseId; //consists of Uppercase English characters only
+public class CurrentAccount extends BankAccount {
+    String tradeLicenseId; // consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, balance, 5000); // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        if (balance < 5000) {
+            throw new Exception("Insufficient Balance");
+        }
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +17,20 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        if (!isValidLicenseId(tradeLicenseId)) {
+            throw new Exception("Valid License can not be generated");
+        }
     }
 
+    private boolean isValidLicenseId(String tradeLicenseId) {
+        // Implement logic to validate trade license ID
+        // Return true if valid, false otherwise
+        // Example: return isValid;
+        for (int i = 0; i < tradeLicenseId.length() - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
